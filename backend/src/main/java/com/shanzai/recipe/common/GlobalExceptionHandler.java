e93d7f908f0d42e95312382c58e2fa0cc1e3a9e1
xgmx2005 <xgmx2005@users.noreply.github.com>
2026-07-07 10:56:56 +0800

    feat: 添加后端统一响应和异常处理

---
+++ backend/src/main/java/com/shanzai/recipe/common/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.shanzai.recipe.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(BusinessException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse<Void> handleBusinessException(BusinessException exception) {
+        return ApiResponse.fail(exception.getMessage());
+    }
+
+    @ExceptionHandler({MethodArgumentNotValidException.class, BindException.class})
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiResponse<Void> handleValidationException(Exception exception) {
+        return ApiResponse.fail("请求参数不正确");
+    }
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public ApiResponse<Void> handleException(Exception exception) {
+        return ApiResponse.fail("系统异常，请稍后重试");
+    }
+}
---
+++ backend/src/main/java/com/shanzai/recipe/common/Role.java
@@ -0,0 +1,6 @@
+package com.shanzai.recipe.common;
+
+public enum Role {
+    USER,
+    MAINTAINER
+}
